5fb0591de1290b0275db6369de24b744d7546946
Dogitachi <takashiminamoto14@gmail.com>
2024-11-03 21:43:30 -0500

    Update ServerConstants.cs

--- Items.Web/ServerConstants.cs
+++ Items.Web/ServerConstants.cs
@@ -3,7 +3,6 @@
     public static class ServerConstants
     {
         public const string UpdateError = "Error updating item.";
-        public const string DeleteError = "Error deleting item.";
-        
+        public const string DeleteError = "Error deleting item.";   
     }
 }
